4cd941a39bb7b9736d14997dd88702d95f7f338d
Diego Nobre <diego@voxtecnologia.com.br>
2015-12-03 23:02:57 -0300

    #4 Installed ornicar/gravatar-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            //new Custom\CMSBundle\CustomCMSBundle(),
+            new Ornicar\GravatarBundle\OrnicarGravatarBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
