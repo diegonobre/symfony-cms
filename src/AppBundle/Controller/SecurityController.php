1c207a9a84588a933a36a0f12e8913dae618531c
Diego Nobre <dcnobre@gmail.com>
2016-01-04 23:43:35 -0300

    Removed unused parameter

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -12,7 +12,7 @@ class SecurityController extends Controller
     /**
      * @Route("/login", name="login-route")
      */
-    public function loginAction(Request $request)
+    public function loginAction()
     {
         $authenticationUtils = $this->get('security.authentication_utils');
 
